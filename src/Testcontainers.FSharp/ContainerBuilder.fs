363b74c5a13c8a3d9b5d3d6205c6149c156647d8
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-12 19:11:05 +0100

    small changes

--- src/Testcontainers.FSharp/ContainerBuilder.fs
+++ src/Testcontainers.FSharp/ContainerBuilder.fs
@@ -1,12 +1,14 @@
 namespace Testcontainers.FSharp
 
+open System.Collections.Generic
 open DotNet.Testcontainers.Builders
 open System
+open DotNet.Testcontainers.Configurations
 
 [<AutoOpen>]
 module Container =
     type FsharpContainerBuilder internal () =
-        member _.Yield() =
+        member _.Yield(_) =
             ContainerBuilder()
               
         member _.Run(state: ContainerBuilder) = state.Build()
@@ -14,10 +16,18 @@ module Container =
         [<CustomOperation("command")>]
         member _.Cmd (state: ContainerBuilder, commands: string[]) =
             state.WithCommand(commands)
+            
+        [<CustomOperation("env")>]
+        member _.Env (state: ContainerBuilder, envs: IReadOnlyDictionary<String, String>) =
+            state.WithEnvironment(envs)
 
-        [<CustomOperation("images")>]
+        [<CustomOperation("image")>]
         member _.Metadata (state: ContainerBuilder, image: String) =
             state.WithImage(image)
+        
+        [<CustomOperation("waitStrategy")>]
+        member _.WaitUntil(state: ContainerBuilder, wait: IWaitForContainerOS) =
+            state.WithWaitStrategy(wait)
     
     let container = FsharpContainerBuilder()
 
--- tests/Testcontainers.FSharp.Tests/Tests.fs
+++ tests/Testcontainers.FSharp.Tests/Tests.fs
@@ -1,5 +1,7 @@
 module Tests
 
+open System.Threading
+open DotNet.Testcontainers.Builders
 open Testcontainers.FSharp
 open Xunit
 open FsUnit.Xunit
@@ -7,7 +9,31 @@ open FsUnit.Xunit
 [<Fact>]
 let ``Test alpine container`` () =
     let alpine = container {
-        cmd "echo"
+        image "alpine:3.17"
+        env readOnlyDict[("TEST", "2")]
     }
     
-    alpine |> should not' null
+    alpine.Image.Name |> should equal "alpine"
+    alpine.Image.Tag |> should equal "3.17"
+
+
+[<Fact>]
+let ``Test run alpine container`` () =
+    let alpine = container {
+        image "alpine:3.17"
+        env readOnlyDict[("TEST", "2")]
+        waitStrategy Wait.ForUnixContainer()
+    }
+    
+    alpine.Image.Name |> should equal "alpine"
+    alpine.Image.Tag |> should equal "3.17"
+    
+    task {
+        do! alpine.StartAsync()
+        
+        let! subject = alpine.ExecAsync([|"echo"; "Test"|], CancellationToken.None)
+        
+        subject.ExitCode |> should equal 0
+        subject.Stdout |> should equal "Test"
+        do! alpine.StopAsync()
+    }
